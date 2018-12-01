c06baed220392e48156d0257da3443bb02f5f7d9
bantyK <bantyp92@gmail.com>
2018-12-01 11:55:24 +0530

    First beginner problem code changes

--- codechef/EventSolution.java
+++ codechef/EventSolution.java
@@ -1,9 +1,10 @@
 import java.util.HashMap;
+import java.util.Scanner;
 
 // https://www.codechef.com/problems/EVENT
 
-public class EventSolution {
-    
+class EventSolution {
+
     static int minNumberOfDays(String startDay, String endDay) {
         HashMap<String, Integer> map = new HashMap<>();
         map.put("sunday", 7);
@@ -18,7 +19,7 @@ public class EventSolution {
         int end = map.get(endDay);
         int minDays = 0;
         int diff = start - end;
-        if(diff >= 0) {
+        if (diff >= 0) {
             minDays = diff + 1;
         } else {
             minDays = 7 + diff + 1;
@@ -28,39 +29,65 @@ public class EventSolution {
     }
 
     static void getNumberOfDays(int minDays, int L, int R) {
-        int range = R - L + 1;
-        int min = minDays;
-        if(min > R) {
-            System.out.println("impossible");
-            return;
-        } else {
-            int count = 0;
-            while(min <= range) {
-                min += 8;
-                if(min > range) {
-                    System.out.println("impossible");
-                    return;
-                } else {
-                    count += 1;
-                    if(count > 1) {
-                        System.out.println("many");
-                        return;
-                    }
-                }
-            }
-        
-            if(count == 1) {
-                System.out.println(minDays);
+        int start = getStart(minDays, L);
+        int count = 0;
+        while (start <= R) {
+            count += 1;
+            start += 7;
+
+            if (count > 1) {
+                System.out.println("many");
+                return;
             }
         }
-    }
-    
-    public static void main(String[] args) {
-        int min = minNumberOfDays("saturday", "sunday");
-        // System.out.println(minNumberOfDays("sunday", "saturday"));
-        // System.out.println(minNumberOfDays("monday", "wednesday"));
-        // System.out.println(minNumberOfDays("wednesday", "monday"));
 
-        getNumberOfDays(min, 3, 5);
+        if (count == 1) {
+            System.out.println(minDays);
+        } else {
+            System.out.println("impossible");
+        }
+    }
+
+    static void printPossibleDays(String startDay, String endDay, int L, int R) {
+        int min = minNumberOfDays(startDay, endDay);
+        getNumberOfDays(min, L, R);
+    }
+
+    static int getStart(int min, int L) {
+        if (min >= L)
+            return min;
+        else
+            return min + 7;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int testCases = scanner.nextInt();
+        scanner.nextLine();
+        Input[] inputs = new Input[testCases];
+        for (int i = 0; i < testCases; i++) {
+            String line = scanner.nextLine();
+            String[] words = line.split(" ");
+            Input input = new Input(words[0], words[1], Integer.parseInt(words[2]), Integer.parseInt(words[3]));
+            inputs[i] = input;
+        }
+
+        for (int i = 0; i < testCases; i++) {
+            printPossibleDays(inputs[i].startDay, inputs[i].endDay, inputs[i].L, inputs[i].R);
+        }
+    }
+}
+
+class Input {
+    String startDay;
+    String endDay;
+    int L;
+    int R;
+
+    Input(String s, String e, int l, int r) {
+        startDay = s;
+        endDay = e;
+        L = l;
+        R = r;
     }
 }
