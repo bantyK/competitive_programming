8aaffab9f1796f5b033e36234a52b8c3355ed2bf
bantyK <bantyp92@gmail.com>
2019-10-20 10:46:49 +0530

    Valid BST accepted solution added

---
+++ leetcode/Medium/ValidBST.java
@@ -0,0 +1,35 @@
+package solutions.medium;
+
+import models.TreeNode;
+
+public class ValidBST {
+    public static void main(String[] args) {
+        ValidBST obj = new ValidBST();
+
+        TreeNode root = new TreeNode(2147483647);
+//        root.left = new TreeNode(3);
+//        root.right = new TreeNode(6);
+//        root.left.left = new TreeNode(2);
+//        root.left.right = new TreeNode(4);
+//        root.left.left.left = new TreeNode(1);
+
+        if (obj.isValidBST(root)) {
+            System.out.println("Valid");
+        } else {
+            System.out.println("Not valid");
+        }
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        return isValidBSTHelper(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean isValidBSTHelper(TreeNode root, long minValue, long maxValue) {
+        if (root == null) return true;
+
+        return root.val > minValue
+                && root.val < maxValue
+                && isValidBSTHelper(root.left, minValue, root.val)
+                && isValidBSTHelper(root.right, root.val, maxValue);
+    }
+}
