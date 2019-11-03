58e2cd90ea68b5697f1c8ec2f5d8c25c9e127a79
bantyK <bantyp92@gmail.com>
2019-11-03 09:09:44 +0530

    Leetcode#1161: Minimum level with Maximum sum accepted solution added

---
+++ leetcode/Medium/MaximumLevelSum.java
@@ -0,0 +1,15 @@
+package solutions.medium;
+
+import models.TreeNode;
+
+// https://leetcode.com/problems/maximum-level-sum-of-a-binary-tree/
+public class MaximumLevelSum {
+    public static void main(String[] args) {
+        MaximumLevelSum obj = new MaximumLevelSum();
+    }
+
+    public int maxLevelSum(TreeNode root) {
+        return 0;
+    }
+
+}
