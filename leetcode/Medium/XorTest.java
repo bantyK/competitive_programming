deef9e28eb5533065c2935ca0958147040a5b41e
bantyK <bantyp92@gmail.com>
2019-11-05 20:51:18 +0530

    Leetcode#442 duplicate in array accepted solution added

---
+++ leetcode/Medium/XorTest.java
@@ -0,0 +1,23 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class XorTest {
+    public static void main(String[] args) {
+        XorTest obj = new XorTest();
+        List<Integer> duplicates = obj.findDuplicates(new int[]{4, 3, 2, 7, 8, 2, 3, 1});
+        System.out.println(duplicates);
+    }
+
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; ++i) {
+            int index = Math.abs(nums[i])-1;
+            if (nums[index] < 0)
+                res.add(Math.abs(index+1));
+            nums[index] = -nums[index];
+        }
+        return res;
+    }
+}
