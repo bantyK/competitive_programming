eca43289d5cddcec2da517e189976feaea0f29b4
bantyK <bantyp92@gmail.com>
2019-12-26 15:06:48 +0530

    Leetcode#746 one more solution added

--- leetcode/Easy/ClimbingStairsMinCost.java
+++ leetcode/Easy/ClimbingStairsMinCost.java
@@ -19,4 +19,21 @@ public class ClimbingStairsMinCost {
 
         return Math.min(dp[n-1], dp[n-2]);
     }
+
+    public int minCostClimbingStairs2(int[] cost) {
+        int n = cost.length;
+        if (n == 0) return 0;
+
+        int[] dp = new int[n + 2];
+
+        dp[0] = 0;
+        dp[1] = cost[0];
+
+        for (int i = 2; i < dp.length; i++) {
+            int c = (i > n) ? 0 : cost[i - 1];
+            dp[i] = Math.min(dp[i - 1], dp[i - 2]) + c;
+        }
+
+        return dp[dp.length - 1];
+    }
 }
