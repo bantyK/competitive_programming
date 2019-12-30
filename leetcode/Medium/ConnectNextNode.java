b6c8925d368f1d8525e182a7b02bb11bb148689c
bantyK <bantyp92@gmail.com>
2019-12-30 08:52:18 +0530

    Leetcode#117 modified

--- leetcode/Medium/ConnectNextNode.java
+++ leetcode/Medium/ConnectNextNode.java
@@ -1,38 +1,68 @@
 import java.util.*;
 
-//117 https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/
 public class ConnectNextNode {
     public static void main(String[] args) {
         ConnectNextNode obj = new ConnectNextNode();
+
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        Node res = obj.connect(root);
+        System.out.println(res);
     }
 
     public Node connect(Node root) {
-        if(root == null) return null;
+        Node current = root;
+        Node head = null;
+        Node prev = null;
 
-        Queue<Node> queue = new LinkedList<>();
-        queue.offer(root);
-
-        while(!queue.isEmpty()) {
-            int n = queue.size();
-            List<Node> nodes = new ArrayList<>();
-            for(int i = 0; i < n; i++) {
-                Node curr = queue.poll();
-
-                nodes.add(curr);
-
-                if(curr.left != null) {
-                    queue.offer(curr.left);
+        while (current != null) {
+            // processing the current level
+            while (current != null) {
+                if (current.left != null) {
+                    if (prev != null) {
+                        prev.next = current.left;
+                    } else {
+                        head = current.left;
+                    }
+                    prev = current.left;
                 }
 
-                if(curr.right != null) {
-                    queue.offer(curr.right);
+                if (current.right != null) {
+                    if (prev != null) {
+                        prev.next = current.right;
+                    } else {
+                        head = current.right;
+                    }
+                    prev = current.right;
                 }
-            }
 
-            for(int i = 0; i < nodes.size() - 1; i++) {
-                nodes.get(i).next = nodes.get(i + 1);
+                current = current.next;
             }
+            current = head;
+            head = null;
+            prev = null;
         }
+
         return root;
+    }
+
+
+    static class Node {
+        int val;
+        Node left;
+        Node right;
+        Node next;
+
+        public Node(int val) {
+            this.val = val;
+        }
     }
 }
