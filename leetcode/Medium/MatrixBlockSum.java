3a2b160e0a4695c81e21f65d741db8aa1703a216
bantyK <bantyp92@gmail.com>
2020-01-11 21:40:04 +0530

    Leetcode#5144

---
+++ leetcode/Medium/MatrixBlockSum.java
@@ -0,0 +1,20 @@
+// 5144 https://leetcode.com/problems/matrix-block-sum/
+public class MatrixBlockSum{
+	public int sumEvenGrandparent(TreeNode root) {
+        	if (root == null) return 0;
+	        int totalSum = 0;
+        	if (root.val % 2 == 0) {
+	            totalSum = getSum(root.left) + getSum(root.right);
+       		 }
+
+        return totalSum + sumEvenGrandparent(root.left) + sumEvenGrandparent(root.right);
+    }
+
+    private int getSum(TreeNode left) {
+        if (left == null) return 0;
+        int sum = 0;
+        sum += left.left != null ? left.left.val : 0;
+        sum += left.right != null ? left.right.val : 0;
+        return sum;
+    }
+}
