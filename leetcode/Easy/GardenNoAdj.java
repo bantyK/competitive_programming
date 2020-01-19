c5f039dceb7aa2d3a6564b4a3781bf96449495f4
bantyK <bantyp92@gmail.com>
2020-01-19 22:05:33 +0530

    Leetcode#1042: graph coloring

--- leetcode/Easy/GardenNoAdj.java
+++ leetcode/Easy/GardenNoAdj.java
@@ -3,6 +3,40 @@ import java.util.*;
 // 1042 https://leetcode.com/problems/flower-planting-with-no-adjacent/
 public class GardenNoAdj {
 
+	public int[] coloring(int n, int[][] paths) {
+
+ 	       Map<Integer, Set<Integer>> graph = new HashMap<>();
+	
+       	 for (int i = 0; i < n; i++) graph.put(i, new HashSet<>());
+
+        for (int[] path : paths) {
+            final int x = path[0] - 1;
+            final int y = path[1] - 1;
+
+            graph.get(x).add(y);
+            graph.get(y).add(x);
+        }
+
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            int[] colors = new int[5];
+
+            for (int nei : graph.get(i)) {
+                final int idx = res[nei];
+                colors[idx] = 1;
+            }
+
+            for (int c = 1; c <= 4; c++) {
+                if (colors[c] != 1) {
+                    res[i] = c;
+                    break;
+                }
+            }
+        }
+
+        return res;
+    }
+
     public int[] gardenNoAdj(int n, int[][] paths) {
         Map<Integer, List<Integer>> graph = new HashMap<>();
 
