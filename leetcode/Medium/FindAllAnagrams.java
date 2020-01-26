5ed92a5dcd329aa3fd5653d8dc8e94226265d7cd
bantyK <bantyp92@gmail.com>
2020-01-26 12:22:45 +0530

    Leetcode#438 sliding window technique added

--- leetcode/Medium/FindAllAnagrams.java
+++ leetcode/Medium/FindAllAnagrams.java
@@ -7,6 +7,52 @@ public class FindAllAnagrams {
         System.out.println(obj.findAnagrams("abab","ab"));
     }
 
+
+    /**
+    * Sliding window technique
+    */
+    public List<Integer> findAnagrams2(String s, String p) {
+        List<Integer> result = new ArrayList<>();
+        if (p.length() > s.length()) return result;
+
+        Map<Character, Integer> charMap = new HashMap<>();
+        for (char c : p.toCharArray()) {
+            int prevCount = charMap.getOrDefault(c, 0);
+            charMap.put(c, prevCount + 1);
+        }
+        int size = charMap.size();
+
+        int left = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char rightChar = s.charAt(right);
+            if (charMap.containsKey(rightChar)) {
+                charMap.put(rightChar, charMap.get(rightChar) - 1);
+                if (charMap.get(rightChar) == 0) {
+                    size--;
+                }
+            }
+
+
+            while (size == 0) {
+                if (right - left + 1 == p.length()) {
+                    result.add(left);
+                }
+
+
+                char leftChar = s.charAt(left);
+                if (charMap.containsKey(leftChar)) {
+                    charMap.put(leftChar, charMap.get(leftChar) + 1);
+                    if (charMap.get(leftChar) > 0)
+                        size++;
+                }
+                left++;
+            }
+        }
+
+        return result;
+    }
+
     public List<Integer> findAnagrams(String b, String a) {
         int lenA = a.length();
         int lenB = b.length();
