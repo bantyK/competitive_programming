75d93f395e5c1401faf94446df1b5e6c8091cb48
bantyK <bantyp92@gmail.com>
2020-02-22 14:47:08 +0530

    Leetcode#992

--- leetcode/Medium/sliding-window/GoodSubarrays.java
+++ leetcode/Medium/sliding-window/GoodSubarrays.java
@@ -10,8 +10,56 @@ public class GoodSubarrays {
         GoodSubarrays obj = new GoodSubarrays();
         int[] a = new int[]{1, 2, 1, 2, 3};
         int k = 2;
-        int num = obj.subarraysWithKDistinct(a, k);
+        int num = obj.subarraysWithKDistinct2(a, k);
         System.out.println(num);
+    }
+
+    /**
+     * Approach 2: Calculate the number of subarrays with ATMOST K distinct elements and number of subarrays with ATMOST (K - 1) distinct elements.
+     * The difference between the two will give the number of subarrays with exactly k distinct elements.
+     *
+     * @param a
+     * @param k
+     * @return
+     */
+    public int subarraysWithKDistinct2(int[] a, int k) {
+        int res1 = subarraysWithAtmostKDistinct(a, k);
+        System.out.println(res1);
+        int res2 = subarraysWithAtmostKDistinct(a, k - 1);
+        System.out.println(res2);
+        return res1 - res2;
+    }
+
+
+    /**
+     * @param a
+     * @param k
+     * @return the number of subarrays with ATMOST K distinct elements
+     */
+    private int subarraysWithAtmostKDistinct(int[] a, int k) {
+        int left = 0;
+        int res = 0;
+        int right = 0;
+
+        Map<Integer, Integer> countMap = new HashMap<>();
+
+        while (right < a.length) {
+            int rightNum = a[right];
+            countMap.put(rightNum, countMap.getOrDefault(rightNum, 0) + 1);
+
+            while (countMap.size() > k) {
+                int leftNum = a[left++];
+                countMap.put(leftNum, countMap.get(leftNum) - 1);
+                if (countMap.get(leftNum) == 0) {
+                    countMap.remove(leftNum);
+                }
+            }
+
+            res += right - left + 1;
+            right++;
+        }
+
+        return res;
     }
 
     public int subarraysWithKDistinct(int[] A, int K) {
