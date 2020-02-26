51e6df7f6b1ff477e2302c6972d338ca0846f0c7
bantyK <bantyp92@gmail.com>
2020-02-26 21:05:21 +0530

    Leetcode#650

--- leetcode/Medium/dp/KeyboardKeys.java
+++ leetcode/Medium/dp/KeyboardKeys.java
@@ -4,12 +4,36 @@ import java.util.*;
 public class KeyboardKeys {
     public static void main(String[] args) {
         KeyboardKeys obj = new KeyboardKeys();
-        System.out.println(obj.minSteps(1000));
+        System.out.println(obj.minSteps(100));
     }
 
     public int minSteps(int n) {
         Map<String, Integer> cache = new HashMap<>();
-        return helper(n, "", "A", 0, cache);
+//        return helper(n, "", "A", 0, cache);
+        Integer[][] dp = new Integer[n + 1][n + 1];
+        return countSteps(n, 0, 1, 0, dp);
+    }
+
+    private int countSteps(int n, int clipBoard, int document, int steps, Integer[][] cache) {
+        if (document == n) {
+            return steps;
+        }
+
+        if (document > n || clipBoard > n) return Integer.MAX_VALUE;
+        if (cache[clipBoard][document] != null) return cache[clipBoard][document];
+
+        int copy = Integer.MAX_VALUE;
+        int paste = Integer.MAX_VALUE;
+
+        if (document != clipBoard) {
+            copy = countSteps(n, document, document, steps + 1, cache);
+        }
+        // paste -> clipboard will remain same and document's content will be appended with clipboard's content
+        if (clipBoard > 0)
+            paste = countSteps(n, clipBoard, document + clipBoard, steps + 1, cache);
+
+        cache[clipBoard][document] = Math.min(copy, paste);
+        return cache[clipBoard][document];
     }
 
     private int helper(int n, String clipBoard, String document, int steps, Map<String, Integer> cache) {
