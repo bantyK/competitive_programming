0c3dceb2220173f66a843f9b9b56d135238263a2
bantyK <bantyp92@gmail.com>
2020-03-06 08:13:24 +0530

    Leetcode#1358- sliding window explainantion added

--- leetcode/Medium/sliding-window/SubstringContainingAllThreeChars.java
+++ leetcode/Medium/sliding-window/SubstringContainingAllThreeChars.java
@@ -1,4 +1,13 @@
 //1358 https://leetcode.com/problems/number-of-substrings-containing-all-three-characters/
+
+/**
+* If at any index-pair(i, j) the constraint is met, then all the indices after j will form a substring containting(at-least) 
+* one character each. These are the substrings which are formed to the left index j. (eg: abcaaaaa)
+
+* Similarly it is possible that there are multiple instances of each character inside (i, j) so in order to count those we
+* need to make the window smaller and check the same constaints, if the constaint is met, we will count those substrings also
+* eg(aaaabc)
+*/
 public class SubstringContainingAllThreeChars {
     public static void main(String[] args) {
         SubstringContainingAllThreeChars obj = new SubstringContainingAllThreeChars();
