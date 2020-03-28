58070ebb0f0a8806551ca9d72fece1c1485f1074
bantyK <bantyp92@gmail.com>
2020-03-28 12:36:19 +0530

    Leetcode#173 : do not push unless you have a solution which beats more than 80% of submissions

--- leetcode/Medium/BSTIterator.java
+++ leetcode/Medium/BSTIterator.java
@@ -3,38 +3,34 @@ import input.TreeNode;
 import java.util.*;
 
 // 173 https://leetcode.com/problems/binary-search-tree-iterator/
-public class BSTIterator {
-    List<TreeNode> path = new ArrayList<>();
-
-    public static void main(String[] args) {
-        BSTIterator obj = new BSTIterator();
+class BSTIterator {
+    
+    Stack<TreeNode> stack;
+    
+    public BSTIterator(TreeNode root) {
+        stack = new Stack<>();
+        
+        TreeNode node = root;
+        while(node != null) {
+            stack.push(node);
+            node = node.left;
+        }
     }
-
-    /**
-     * Find the next smallest element, stop when a node does not have a left child
-     *
-     * @param root
-     * @param path
-     */
-    public void dfs(TreeNode root, List<TreeNode> path) {
-        if (root == null) return;
-        path.add(root);
-        dfs(root.left, path);
-    }
-
-    /**
-     * @return the next smallest number
-     */
+    
+    /** @return the next smallest number */
     public int next() {
-        TreeNode lastElement = path.remove(path.size() - 1);
-        dfs(lastElement.right, path);
-        return lastElement.val;
+        TreeNode node = stack.pop();
+        TreeNode right = node.right;
+        while(right != null) {
+            stack.push(right);
+            right = right.left;
+        }
+        
+        return node.val;
     }
-
-    /**
-     * @return whether we have a next smallest number
-     */
+    
+    /** @return whether we have a next smallest number */
     public boolean hasNext() {
-        return path.size() > 0;
+        return !stack.isEmpty();
     }
 }
