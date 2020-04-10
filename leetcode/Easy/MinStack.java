361c149fc34c4057a43425c9ce22124ef0252a42
bantyK <bantyp92@gmail.com>
2020-04-10 23:00:46 +0530

    Leetcode#155

---
+++ leetcode/Easy/MinStack.java
@@ -0,0 +1,53 @@
+// 155 https://leetcode.com/problems/min-stack/
+public class MinStack {
+
+    Node head;
+
+    public MinStack() {
+        head = null;
+    }
+
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        minStack.getMin();
+        minStack.pop();
+        minStack.top();
+        minStack.getMin();
+    }
+
+    public void push(int x) {
+        if (head == null) {
+            head = new Node(x, x, null);
+        } else {
+            head = new Node(x, Math.min(head.min, x), head);
+        }
+    }
+
+    public void pop() {
+        head = head.next;
+    }
+
+    public int top() {
+        return head.val;
+    }
+
+    public int getMin() {
+        return head.min;
+    }
+
+    class Node {
+        int val;
+        int min;
+        Node next;
+
+        public Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+}
+
