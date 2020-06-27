384a3c2a3244c2f51a41daa68bae1593ffbcbbdd
bantyK <bantyp92@gmail.com>
2020-06-27 18:27:51 +0530

    Leetcode#279

--- leetcode/Medium/dp/PerfectSquareSum.java
+++ leetcode/Medium/dp/PerfectSquareSum.java
@@ -10,44 +10,72 @@ public class PerfectSquareSum {
         System.out.println(i);
     }
 
-    public int numSquaresBrute(int n) {
-        List<Integer> perfectSquares = new ArrayList<>();
-        for (int i = 1; i * i <= n; i++) {
-            perfectSquares.add(i * i);
+    import java.util.*;
+
+public class PerfectSquares {
+
+    public static void main(String[] args) {
+        PerfectSquares obj = new PerfectSquares();
+        System.out.println(obj.numSquares(13));
+        System.out.println(obj.numSquares(15));
+        System.out.println(obj.numSquares(18));
+        System.out.println(obj.numSquares(49));
+        System.out.println(obj.numSquares(99));
+        System.out.println(obj.numSquares(5));
+        System.out.println(obj.numSquares(5));
+    }
+
+    public int numSquares(int n) {
+        Map<String, Integer> dp = new HashMap<>();
+        int[] perfectSquares = getSquares(n);
+        return helper(perfectSquares, n);
+    }
+
+    private int helper(int[] nums, int remaining) {
+        int n = nums.length;
+        int[][] dp = new int[n][remaining + 1];
+
+        for(int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
         }
-        List<Integer> current = new ArrayList<>();
-        solve(perfectSquares, current, 0, n);
+
+        for(int i = 0; i < n; i++) {
+            dp[i][0] = 0;
+        }
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 1; j <= remaining; ++j) {
+                if(i > 0) {
+                    dp[i][j] = dp[i-1][j];
+                }
+
+                if(j >= nums[i]) {
+                    if(dp[i][j - nums[i]] != Integer.MAX_VALUE) {
+                        dp[i][j] = Math.min(dp[i][j], 1 + dp[i][j - nums[i]]);
+                    }
+                }
+            }
+        }
+        return dp[n-1][remaining];
+    }
+
+    private int[] getSquares(int n) {
+        int[] res = new int[(int) Math.sqrt(n)];
+        int idx = 0;
+        for (int i = 1; i <= n; ++i) {
+            if (isPerfectSquare(i)) {
+                res[idx++] = i;
+            }
+        }
+
         return res;
     }
 
-    private void solve(List<Integer> perfectSquares, List<Integer> current, int index, int currentSum) {
-        if (currentSum == 0) {
-            System.out.println(current);
-            int n = current.size();
-            res = Math.min(res, n);
-            return;
-        }
 
-        for (int i = index; i < perfectSquares.size(); i++) {
-            if (perfectSquares.get(i) <= currentSum) {
-                current.add(perfectSquares.get(i));
-                solve(perfectSquares, current, i, currentSum - perfectSquares.get(i));
-                current.remove(current.size() - 1);
-            }
-        }
+    private boolean isPerfectSquare(int x) {
+        double sr = Math.sqrt(x);
+        return ((sr - Math.floor(sr)) == 0);
     }
+}
 
-    private int numSquares(int n) {
-        if (n <= 0) return 0;
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[0] = 0;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j * j <= i; j++) {
-                dp[i] = Math.min(dp[i], 1 + dp[i - j * j]);
-            }
-        }
-        System.out.println(Arrays.toString(dp));
-        return dp[n];
-    }
 }
