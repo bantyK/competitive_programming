3c094d1dec48590a1dc3faac6ddacf80b37a407c
bantyK <bantyp92@gmail.com>
2020-09-18 00:00:29 +0530

    atcoder dp solutions added

--- techniques/atcoder-dp/StoneGame.java
+++ techniques/atcoder-dp/StoneGame.java
@@ -18,12 +18,18 @@ public class StoneGame {
         }
 
         /*
-        if dp[i]
+        if dp[i] is true that means the first player which picks i stones will win.
          */
         boolean[] dp = new boolean[k + 1];
 
         for (int stones = 0; stones <= k; stones++) {
             for (int x : nums) {
+                // stones - x means the remaining stones which remains after  picking x stones.
+                // The other player has stones - x stones to pick now.
+                // if dp[stones - x] is true that means the player which has to pick this many stones wins.
+                // In this scenario it was the second player. Hence the first player who picked x stones before lost.
+                // Vice versa, if the dp[stones - x] is false that means for second player who has to pick this many stones will lose
+                // meaning the first player will win
                 if (stones >= x && !dp[stones - x]) {
                     dp[stones] = true;
                     break;
