6e17984273590a220cbffedb608a914547d18786
bantyK <bantyp92@gmail.com>
2020-10-15 23:29:36 +0530

    Leetcode#951

--- leetcode/Medium/FlipEquivalentTree.java
+++ leetcode/Medium/FlipEquivalentTree.java
@@ -4,47 +4,50 @@ import java.util.*;
 // 951 https://leetcode.com/problems/flip-equivalent-binary-trees/
 public class FlipEquivalentTree {
     public boolean flipEquiv(TreeNode root1, TreeNode root2) {
-        if(root1 == null && root2 == null) return true;
-        if(root1 == null || root2 == null) return false;
+        if (root1 == null) return root2 == null;
+        if (root2 == null) return false;
 
-        Queue<TreeNode> q1 = new LinkedList<>();
-        Queue<TreeNode> q2 = new LinkedList<>();
+        Queue<TreeNode> queue1 = new LinkedList<>();
+        Queue<TreeNode> queue2 = new LinkedList<>();
 
-        q1.offer(root1);
-        q2.offer(root2);
+        queue1.offer(root1);
+        queue2.offer(root2);
 
-        while(!q1.isEmpty() && !q2.isEmpty()) {
-            TreeNode r1 = q1.poll();
-            TreeNode r2 = q2.poll();
+        while (!queue1.isEmpty() && !queue2.isEmpty()) {
+            TreeNode node1 = queue1.poll();
+            TreeNode node2 = queue2.poll();
+            
+            
+            if(node1.val != node2.val) return false;
 
-            if(equal(r1.left, r2.left) && equal(r1.right, r2.right)) {
-                add(q1, r1.left);
-                add(q1, r1.right);
-                add(q2, r2.left);
-                add(q2, r2.right);
-            } else if(equal(r1.left, r2.right) && equal(r1.right, r2.left)) {
-                add(q1, r1.left);
-                add(q2, r2.right);
-                add(q1, r1.right);
-                add(q2, r2.left);
+            if (equal(node1.left, node2.left) && equal(node1.right, node2.right)) {
+                add(queue1, node1.left);
+                add(queue1, node1.right);
+                add(queue2, node2.left);
+                add(queue2, node2.right);
+            } else if (equal(node1.left, node2.right) && equal(node1.right, node2.left)) {
+                add(queue1, node1.left);
+                add(queue1, node1.right);
+                add(queue2, node2.right);
+                add(queue2, node2.left);
             } else {
                 return false;
             }
         }
 
-        return q1.size() == q2.size();
+        return queue1.size() == 0 && queue2.size() == 0;
 
+    }
+
+    private void add(Queue<TreeNode> queue, TreeNode node) {
+        if (node != null) queue.offer(node);
     }
 
     private boolean equal(TreeNode node1, TreeNode node2) {
-        if(node1 == null) return node2 == null;
-        if(node2 == null) return false;
+        if (node1 == null) return node2 == null;
+        if (node2 == null) return false;
 
         return node1.val == node2.val;
-    }
-
-    private void add(Queue<TreeNode> q, TreeNode n) {
-        if(n != null) q.offer(n);
     }
 
 }
