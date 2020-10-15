135ec0b6f5cce640398fbf282e28de6b3bd81cae
bantyK <bantyp92@gmail.com>
2020-10-15 07:56:40 +0530

    Leetcode#213 modified code

--- leetcode/Medium/dp/HouseRobber2.java
+++ leetcode/Medium/dp/HouseRobber2.java
@@ -1,41 +1,44 @@
-package fibonaci;
+import java.util.Arrays;
+import java.util.Random;
 
 //213 https://leetcode.com/problems/house-robber-ii/
 public class HouseRobber2 {
+    public static void main(String[] args) {
+        HouseRobber2 obj = new HouseRobber2();
+        System.out.println(obj.rob(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}));
+        int[] nums = new int[100];
+        for(int i = 0; i < 100; i++)
+            nums[i] = new Random().nextInt(1000);
+
+        System.out.println(Arrays.toString(nums));
+        System.out.println(obj.rob(nums));
+    }
 
     public int rob(int[] nums) {
-        if (nums.length == 0) return 0;
-        if (nums.length == 1) return nums[0];
-        if (nums.length == 2) return Math.max(nums[0], nums[1]);
+        // maximum amount if we rob the first house
+        int takeFirstHouse = rob(nums, 0, nums.length - 2);
 
-        int first = first(nums, 0, nums.length - 2);
-        int second = second(nums, 1, nums.length - 1);
+        // maximum amount if we rob the second house
+        int takeLastHouse = rob(nums, 1, nums.length - 1);
 
-        return Math.max(first, second);
+        return Math.max(takeFirstHouse, takeLastHouse);
     }
 
-    private int first(int[] nums, int startIndex, int lastIndex) {
-        int[] dp = new int[nums.length - 1];
-        dp[0] = nums[startIndex];
-        dp[1] = Math.max(nums[startIndex], nums[startIndex + 1]);
-
-        for (int i = 2; i < dp.length; i++) {
-            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
-        }
-
-        return dp[lastIndex];
-    }
-
-    private int second(int[] nums, int startIndex, int lastIndex) {
+    private int rob(int[] nums, int start, int end) {
+        int len = end - start + 1;
         int[] dp = new int[nums.length];
-        dp[0] = 0;
-        dp[1] = nums[startIndex];
-        dp[2] = Math.max(nums[startIndex], nums[startIndex + 1]);
+        if (start >= 0) {
+            dp[start] = nums[start];
+        }
+        if (len >= 2) {
+            dp[start + 1] = Math.max(nums[start], nums[start + 1]);
+        }
 
-        for (int i = 2; i < dp.length; i++) {
+        for (int i = start + 2; i <= end; i++) {
             dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
         }
 
-        return dp[lastIndex];
+        return dp[start + len - 1];
     }
+
 }
