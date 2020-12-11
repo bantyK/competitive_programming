55ecf9d25659c949701cb65175817f1ffb347f77
bantyK <bantyp92@gmail.com>
2020-12-11 17:29:54 +0530

    Leetcode#80

--- leetcode/Medium/RemoveDuplicates2.java
+++ leetcode/Medium/RemoveDuplicates2.java
@@ -2,7 +2,7 @@
 public class RemoveDuplicates2 {
 	public int removeDuplicates(int[] nums) {
 		int i = 0;
-		
+
 		for(int n : nums) {
 			if(i < 2 || n > nums[i]) {
 				nums[i] = n;
@@ -11,4 +11,26 @@ public class RemoveDuplicates2 {
 		}
 		return i;
 	}
+
+	// Another solution
+	// Over-write the position which has more than 2 duplicates
+	// In implementation we over-write everytime the count is less than 2
+	// if the count of any number goes beyond 2, we dont over-write and wait for a number whose
+	// count is less than 2
+	public int removeDuplicates2(int[] nums) {
+        int j = 1, count = 1;
+
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] == nums[i - 1]) {
+                count++;
+            } else {
+                count = 1;
+            }
+
+            if(count <= 2) {
+                nums[j++] = nums[i];
+            }
+        }
+        return j;
+    }
 }
