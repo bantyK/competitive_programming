e72f2fdcd6bd340e26140a3fc7a517c3b209916e
bantyK <bantyp92@gmail.com>
2021-02-06 13:31:35 +0530

    Leetcode#1087

--- leetcode/Medium/BraceExpansion.java
+++ leetcode/Medium/BraceExpansion.java
@@ -5,50 +5,81 @@ public class BraceExpansion {
     public static void main(String[] args) {
         BraceExpansion obj = new BraceExpansion();
 
-        System.out.println(Arrays.toString(obj.expand("{a,b}c{d,ex}")));
+        System.out.println(Arrays.toString(obj.expand("{a,b}c{d,e}f")));
     }
+
+
+    private List<String> res;
 
     public String[] expand(String s) {
-        List<String> buckets = new ArrayList<>();
-
-        final int len = s.length();
-        for (int i = 0; i < len; i++) {
-            StringBuilder builder = new StringBuilder();
-            if (s.charAt(i) == '{') {
-                int j = i + 1;
-                while (j < len && s.charAt(j) != '}') {
-                    if (s.charAt(j) == ',') {
-                        j++;
-                        continue;
-                    }
-                    builder.append(s.charAt(j));
-                    j++;
-                }
-                buckets.add(builder.toString());
-                i = j;
-            } else {
-                buckets.add(s.charAt(i) + "");
-            }
-
-
+        res = new ArrayList<>();
+        dfs(s, 0, new StringBuilder());
+        String[] out = new String[res.size()];
+        int i = 0;
+        for (String ss : res) {
+            out[i++] = ss;
         }
-        List<String> result = new ArrayList<>();
-        dfs(buckets, result, new StringBuilder(), 0);
-
-        Collections.sort(result);
-        return result.toArray(new String[0]);
+        return out;
     }
 
-    private void dfs(List<String> buckets, List<String> result, StringBuilder builder, int index) {
-        if (builder.length() == buckets.size()) {
-            result.add(builder.toString());
+    private void dfs(String s, int index, StringBuilder builder) {
+        if (index == s.length()) {
+            res.add(builder.toString());
             return;
         }
 
-        for (char c : buckets.get(index).toCharArray()) {
-            builder.append(c);
-            dfs(buckets, result, builder, index + 1);
-            builder.setLength(builder.length() - 1);
+        char ch = s.charAt(index);
+
+        if (ch == '{') {
+            List<Character> letters = new ArrayList<>();
+            int endIndex = index + 1;
+            while (endIndex < s.length() && s.charAt(endIndex) != '}') {
+                if (Character.isLetter(s.charAt(endIndex))) {
+                    letters.add(s.charAt(endIndex));
+                }
+                endIndex++;
+            }
+
+            Collections.sort(letters);
+            int prevLen = builder.length();
+
+            for (char letter : letters) {
+                builder.append(letter);
+                // recurse to the next bucket in the string
+                dfs(s, endIndex + 1, builder);
+                // delete all the chars added to the builder before making this recursive call
+                builder.setLength(prevLen);
+            }
+        } else if (Character.isLetter(ch)) {
+            builder.append(ch);
+            dfs(s, index + 1, builder);
+        }
+    }
+
+    public List<String> expand2(String s) {
+        List<String> res = new ArrayList<>();
+        List<List<String>> buckets = new ArrayList<>();
+
+        generateWords(buckets, 0, 0, res, new LinkedList<String>());
+
+        System.out.println(res);
+        return res;
+    }
+
+    private void generateWords(List<List<String>> buckets, int bucketIndex, int index, List<String> res, LinkedList<String> builder) {
+        if (bucketIndex == buckets.size()) {
+            res.add(builder.toString());
+            return;
+        }
+
+        for (int i = bucketIndex; i < buckets.size(); i++) {
+            while (index < buckets.get(bucketIndex).size()) {
+                builder.add(buckets.get(bucketIndex).get(index));
+                generateWords(buckets, bucketIndex + 1, 0, res, builder);
+                index++;
+                builder.removeLast();
+            }
+//            builder.removeLast();
         }
     }
 
