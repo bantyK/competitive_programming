a99657131b5316ebfc6cde730c781b878133da81
bantyK <bantyp92@gmail.com>
2021-04-20 11:11:02 +0530

    LC#377

--- leetcode/Medium/CombinationSum4.java
+++ leetcode/Medium/CombinationSum4.java
@@ -1,38 +1,28 @@
-import java.util.*;
+import java.util.Arrays;
 
-// 377 https://leetcode.com/problems/combination-sum-iv/
+//377 https://leetcode.com/problems/combination-sum-iv/
 public class CombinationSum4 {
+
     public static void main(String[] args) {
-        CombinationSum4 obj = new CombinationSum4();
-        System.out.println(obj.combinationSum4(new int[]{1, 2, 3}, 4));
+        int[] nums = new int[]{3, 2, 1};
+        System.out.println(new CombinationSum4().combinationSum4(nums, 35));
     }
 
     public int combinationSum4(int[] nums, int target) {
+        Arrays.sort(nums);
+
         int[] dp = new int[target + 1];
-        Arrays.fill(dp, -1);
         dp[0] = 1;
-        helper(nums, target, dp);
-        return dp[target];
-    }
 
 
-    // brute force
-    public int helper(int[] nums, int target, int[] dp) {
-        if(dp[target] != -1) return dp[target];
-
-        if(target == 0) {
-            return 1;
-        }
-
-        int res = 0;
-
-        for(int i = 0; i < nums.length; i++) {
-            if(target >= nums[i]) {
-                res += helper(nums, target - nums[i], dp);
+        for (int i = 1; i <= target; i++) {
+            for (int num : nums) {
+                if (num <= i) {
+                    dp[i] += dp[i - num];
+                }
             }
         }
-        dp[target] = res;
-        return res;
-    }
 
+        return dp[target];
+    }
 }
